bd43d8118ad2e76ca6084a106ca93ec90eb630c4
dflavia <dflaviaioana96@gmail.com>
2024-08-11 16:56:36 +0300

    updates

--- HackerRank/4_Basic_Join.sql
+++ HackerRank/4_Basic_Join.sql
@@ -40,7 +40,8 @@ GROUP BY continent
 
 -- The Report
 -- Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8.
--- The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically.
+-- The report must be in descending order by grade -- i.e. higher grades are entered first.
+-- If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically.
 -- Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order.
 -- If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
 
@@ -63,7 +64,8 @@ ORDER BY grade DESC, name, marks
 
 -- Top Competitors
 -- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge.
--- Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+-- Order your output in descending order by the total number of challenges in which the hacker earned a full score.
+-- If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
 -- hackers(hacker_id, name)
 -- difficulty(difficulty_level, score)
 -- challenges(challenge_id, hacker_id, difficulty_level)
@@ -83,7 +85,8 @@ ORDER BY COUNT(submission_id) DESC, h.hacker_id ASC
 -- Ollivander's Inventory
 -- Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
 -- Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age.
--- Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+-- Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power.
+-- If more than one wand has same power, sort the result in order of descending age.
 
 -- wands(id, code, coins_needed, power)
 -- wands_property(code, age, is_evil)
