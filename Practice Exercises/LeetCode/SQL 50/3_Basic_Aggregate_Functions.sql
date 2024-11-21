488f6c39fda9f3fb2f018ddd1979bda83c42d374
dflavia <dflaviaioana96@gmail.com>
2024-11-21 20:53:49 +0200

    updates

--- Practice Exercises/LeetCode/SQL 50/3_Basic_Aggregate_Functions.sql
+++ Practice Exercises/LeetCode/SQL 50/3_Basic_Aggregate_Functions.sql
@@ -67,8 +67,8 @@ ORDER  BY percentage DESC, contest_id;
 
 SELECT
     query_name,
-    ROUND(SUM(rating / position) / COUNT(*),2) AS quality,
-    ROUND(SUM(CASE WHEN rating <3 THEN 1 ELSE 0 END) / COUNT(*) * 100,2) AS poor_query_percentage
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(100.0 * AVG(CASE WHEN rating <3 THEN 1 ELSE 0 END),2) AS poor_query_percentage
 FROM queries
 WHERE query_name IS NOT NULL
 GROUP BY query_name
