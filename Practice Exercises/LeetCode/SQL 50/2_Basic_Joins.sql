c322f2b15cde2e0f1380d77cd6bfef1d9389b89e
dflavia <dflaviaioana96@gmail.com>
2024-11-21 17:18:53 +0200

    updates

--- Practice Exercises/LeetCode/SQL 50/2_Basic_Joins.sql
+++ Practice Exercises/LeetCode/SQL 50/2_Basic_Joins.sql
@@ -124,6 +124,16 @@ WHERE id IN (
              GROUP BY managerid
              HAVING COUNT(managerid)>=5
             )
+
+-- OR
+
+SELECT
+    e1.name
+FROM employee e1
+INNER JOIN employee e2
+    ON e1.id = e2.managerid
+GROUP BY e2.managerid
+HAVING COUNT(e2.managerid) >= 5
             
 
 
