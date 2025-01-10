36695899846a398192e80a0f14c1508918fab26d
dflavia <dflaviaioana96@gmail.com>
2025-01-10 17:41:14 +0200

    updates

--- practice-exercises/LeetCode/SQL 50/3_Basic_Aggregate_Functions.sql
+++ practice-exercises/LeetCode/SQL 50/3_Basic_Aggregate_Functions.sql
@@ -23,7 +23,7 @@ SELECT
      COALESCE(ROUND(SUM(price * units) / SUM(units),2),0) AS average_price
 FROM prices p
 LEFT JOIN unitssold u ON p.product_id = u.product_id
-AND (purchase_date >= start_date AND purchase_date <= end_date)
+AND purchase_date BETWEEN start_date AND end_date
 GROUP BY p.product_id
 
 
